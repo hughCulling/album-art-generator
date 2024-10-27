3772f6731bd6d6eec07e6e5d1aa0a7271f4da878
Hugh Culling <hughculling@icloud.com>
2024-10-27 19:15:06 +0000

    Got transcription working

--- src/app/api/transcribe/route.js
+++ src/app/api/transcribe/route.js
@@ -16,9 +16,36 @@ export const POST = async (request) => {
       return NextResponse.json({ error: "Invalid file type" }, { status: 400 });
     }
 
-    return NextResponse.json({
-      transcription: "Wahey",
-    });
+    try {
+      // Make the API request with the file
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/openai/whisper-large-v3",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HF_ACCESS_TOKEN}`, // Make sure your token is correct
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: file,
+        }
+      );
+
+      // Check if the request was successful
+      if (!response.ok) {
+        console.error(
+          `HTTP Error: ${response.status} - ${response.statusText}`
+        );
+      }
+
+      // Parse and log the response
+      const result = await response.json();
+      console.log("Parsed JSON Response:", result);
+      return NextResponse.json({
+        transcription: result.text,
+      });
+    } catch (error) {
+      console.error("Error during API request:", error);
+    }
   } catch (error) {
     console.error("Error uploading file:", error);
     return NextResponse.json(
